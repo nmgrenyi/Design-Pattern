1d077e46a5122f3cf2085aa8a0c7442af2d0768a
nmgrenyi <Yi_Ren1993@163.com>
2019-04-20 23:38:17 -0700

    add ColdDrink in BuilderPattern

--- src/builderPattern/MealBuilder.java
+++ src/builderPattern/MealBuilder.java
@@ -4,7 +4,9 @@ public class MealBuilder {
 	public Meal prepareVegMeal() {
 		Burger burger = new VegBurger();
 		Meal meal = new Meal();
+		ColdDrink pepsi = new Pepsi();
 		meal.addItem(burger);
+		meal.addItem(pepsi);
 		return meal;
 	}
 	
---
+++ src/builderPattern/Pepsi.java
@@ -0,0 +1,12 @@
+package builderPattern;
+
+public class Pepsi extends ColdDrink{
+	@Override
+	public String name() {
+		return "Pepsi";
+	};
+	@Override
+	public float price() {
+		return 1;
+	};
+}
